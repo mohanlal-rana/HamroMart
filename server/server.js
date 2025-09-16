3bab64420f20d7e8af58948c72cf87a793bdbaed
Mohanlal Rana <mr7569107@gmail.com>
2025-09-16 07:18:30 +0545

    Prepare backend for Render deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://hamromart.netlify.app",
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
 };
